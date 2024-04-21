0ce5accdfe2b78235c538da7419ce670ccd095cd
ThePlatypus-Person <Ornithorhynchus0920@protonmail.com>
2024-04-21 22:25:54 +0700

    Add contract, read with .env, fixed async issue

--- front-end/script.js
+++ front-end/script.js
@@ -1,7 +1,9 @@
+require('dotenv').config();
 const abi = require('./abi.json');
 const Web3 = require('web3');
 
-const contractAddress = "Haven't deployed the contract yet";
+
+const contractAddress = process.env.CONTRACT;
 
 const web3 = new Web3(window.ethereum);
 const contract = new web3.eth.Contract(abi, contractAddress);
@@ -19,7 +21,7 @@ async function connectWallet() {
             });
 
         displayWallet(accounts[0]);
-        // displayOutput();
+        displayOutput();
 
         if (accounts[0])
             console.log("We have an account");
@@ -38,7 +40,15 @@ async function displayOutput() {
     const outputArea = document.querySelector('.output-area');
     outputArea.replaceChildren();
 
-    const peopleList = await getPeopleList();
+    const addressList = await contract.methods.getAllAddress().call();
+    const peopleList = await Promise.all(addressList.map(async addr => {
+        const name = await contract.methods.getPerson(addr).call();
+
+        return {
+            address: addr,
+            name: name,
+        };
+    }));
 
     peopleList.forEach(people => {
         const container = document.createElement('div');
@@ -47,33 +57,40 @@ async function displayOutput() {
         
         addressTitle.textContent = people.address;
         nameTitle.textContent = people.name;
+        container.classList.add('person-container');
         container.append(addressTitle, nameTitle);
+        outputArea.appendChild(container);
+
     });
-}
-
-async function getPeopleList() {
-    const addressList = await contract.methods.getAllAddress().call();
-    const peopleList = await addressList.map(async addr => {
-        const name = await contract.method.getPerson(addr).call();
-
-        peopleList.append({
-            address: addr,
-            name: name,
-        });
-    });
-
-    return peopleList;
 }
 
 async function addPerson(name) {
     const accounts = await web3.eth.getAccounts();
+    const addressList = await contract.methods.getAllAddress().call();
 
     try {
-        await contract.methods.addPerson(name).send({ from: accounts[0] });
-        displayOutput(accounts[0]);
+        let response;
+        if (addressList.includes(accounts[0])) {
+            response = await contract.methods.setPerson(name).send({ from: accounts[0] });
+        } else {
+            response = await contract.methods.addPerson(name).send({ from: accounts[0] });
+        }
     } catch (err) {
         console.log(err);
+    } finally {
+        displayOutput();
     }
 }
 
 connectWallet();
+const nameInput = document.querySelector('input[name=name]');
+const addBtn = document.querySelector('button.submit');
+addBtn.addEventListener('click', async () => {
+    if (!nameInput.value) {
+        console.log("nameInput is empty");
+        return;
+    }
+
+    console.log(nameInput.value);
+    addPerson(nameInput.value);
+});
